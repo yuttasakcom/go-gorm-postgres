07121903eff866e0ee00c33df3a170b050837101
Yo <yuttasakcom@gmail.com>
2022-11-27 03:03:33 +0700

    Create entity account

--- db/account.go
+++ db/account.go
@@ -1,1 +1,16 @@
 package db
+
+type Accounts struct {
+	Owner   string `json:"owner"`
+	Balance int64  `json:"balance"`
+}
+
+func (q *Queries) CreateAccount(arg Accounts) *Accounts {
+	account := &Accounts{
+		Owner:   arg.Owner,
+		Balance: arg.Balance,
+	}
+	q.db.Create(account)
+
+	return account
+}
--- db/db.go
+++ db/db.go
@@ -12,21 +12,6 @@ func New(db *gorm.DB) *Queries {
 	return &Queries{db: db}
 }
 
-type Accounts struct {
-	Owner   string `json:"owner"`
-	Balance int64  `json:"balance"`
-}
-
 type Queries struct {
 	db *gorm.DB
-}
-
-func (q *Queries) CreateAccount(arg Accounts) *Accounts {
-	account := &Accounts{
-		Owner:   arg.Owner,
-		Balance: arg.Balance,
-	}
-	q.db.Create(account)
-
-	return account
 }
